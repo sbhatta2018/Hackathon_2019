a4fa742cc37dfa79ecf0f61e83711d37c565f1d5
sbhatta <sbhatta@zaloni.com>
2019-11-19 20:18:58 +0530

    visaula

---
+++ APPLITOOLS_HACKATHON_PROJECT/src/test/java/com/applitools/visual/ai/approach/base/VisualBaseClass.java
@@ -0,0 +1,87 @@
+package com.applitools.visual.ai.approach.base;
+
+import static com.google.common.base.Strings.isNullOrEmpty;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.applitools.eyes.BatchInfo;
+import com.applitools.eyes.EyesRunner;
+import com.applitools.eyes.selenium.ClassicRunner;
+import com.applitools.eyes.selenium.Eyes;
+
+/**
+ * Base Class to get and close web driver object
+ */
+
+public class VisualBaseClass {
+
+	static WebDriver driver = null;
+	static EyesRunner runner = null;
+	static Eyes eyes = null;
+	static BatchInfo batch = null;
+
+	/**
+	 * This method returns web driver object
+	 *
+	 * @return
+	 */
+	public static WebDriver getDriver() {
+		if (driver == null) {
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + File.separator
+					+ "Chrome_Driver" + File.separator + "chromedriver.exe");
+			driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			driver.manage().window().maximize();
+		}
+		return driver;
+	}
+
+	/**
+	 * This method closes browser
+	 */
+	public static void closeBrowserWindowAndEyes() {
+		if (driver != null) {
+			driver.close();
+		}
+		if (eyes != null) {
+			eyes.abortIfNotClosed();
+		}
+	}
+
+	public static BatchInfo getBatchInfo() {
+		if (batch == null) {
+			batch = new BatchInfo("Hackathon batch");
+		}
+		return batch;
+	}
+
+	public static EyesRunner getClassicRunner() {
+		// Initialize the Runner for your test.
+		if (runner == null) {
+			runner = new ClassicRunner();
+		}
+		return runner;
+	}
+
+	public static Eyes getEyes() {
+		// Initialize the eyes SDK
+		if (eyes == null) {
+
+			eyes = new Eyes(getClassicRunner());
+			// Set your personal Applitols API Key from your environment variables.
+			// Raise an error if no API Key has been found.
+			if (isNullOrEmpty(System.getenv("APPLITOOLS_API_KEY"))) {
+				throw new RuntimeException("No API Key found; Please set environment variable 'APPLITOOLS_API_KEY'.");
+			}
+			eyes.setApiKey(System.getenv("APPLITOOLS_API_KEY"));
+			// set batch name
+			eyes.setBatch(getBatchInfo());
+		}
+		return eyes;
+	}
+
+}
---
+++ APPLITOOLS_HACKATHON_PROJECT/src/test/java/com/applitools/visual/ai/approach/tests/VisualAITests.java
@@ -0,0 +1,222 @@
+package com.applitools.visual.ai.approach.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.applitools.eyes.EyesRunner;
+import com.applitools.eyes.MatchLevel;
+import com.applitools.eyes.RectangleSize;
+import com.applitools.eyes.TestResultsSummary;
+import com.applitools.eyes.selenium.Eyes;
+import com.applitools.eyes.selenium.fluent.Target;
+import com.applitools.visual.ai.approach.base.VisualBaseClass;
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+
+/**
+ * Runs Applitools test for the hackathon app
+ */
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(DataProviderRunner.class)
+public class VisualAITests extends VisualBaseClass {
+
+	private static EyesRunner runner = getClassicRunner();
+	private static Eyes eyes = getEyes();
+	private static WebDriver driver = getDriver();
+	private static TestResultsSummary allTestResults;
+	private static Properties prop;
+	// Page URL's
+	private static String PAGE_URL = System.getProperty("APP_URL");
+	private static String DYNAMIC_URL = System.getProperty("DYNAMIC_URL");
+
+
+	@BeforeClass
+	public static void setUp() {
+
+		prop = new Properties();
+		try {
+			InputStream environmentFile = new FileInputStream(
+					System.getProperty("user.dir") + File.separator + "visual_environment.properties");
+			prop.load(environmentFile);
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+		
+		driver.get(prop.getProperty(PAGE_URL));		
+		
+	}
+
+	// Login page UI Elements test
+	@Test
+	public void test1_LoginPagUIElemenetsTest() {
+
+		// Start the test
+		eyes.open(driver, "Hackathon App", "Login Page UI Elements Test", new RectangleSize(1300, 580));
+
+		// Take a screenshot capturing the login page elements.
+		eyes.checkWindow("Login page UI elements");
+
+		// End the test.
+		eyes.closeAsync();
+
+	}
+
+	// Data Driven Login Functionality Test
+	@Test
+	@UseDataProvider("testData")
+	public void test2_DataDrivenTest(String username, String password, String expected, String testName) {
+
+		login(username, password, testName);
+
+		if ((StringUtils.isEmpty(username)) || (StringUtils.isEmpty(password))) {
+
+			String actual = driver.findElement(By.xpath("/html/body/div/div/div[3]")).getText();
+			assertEquals(testName, expected, actual);
+
+		} else {
+			String loggedUserName = driver.findElement(By.id("logged-user-name")).getText();
+			assertEquals(testName, expected, loggedUserName);
+		}
+	}
+
+	// Table Sort Test
+	@Test
+	public void test3_TableSortTest() {
+
+		// Start the test
+		eyes.open(driver, "Hackathon App", "Table Sort Test", new RectangleSize(1300, 580));
+
+		eyes.setForceFullPageScreenshot(true);
+
+		// Take screenshot of the table before sorting
+		eyes.checkWindow(" Table data before sorting ");
+
+		// Click on the AMOUNT header of the "Recent Transactions" table
+		driver.findElement(By.id("amount")).click();
+
+		// Take screenshot of the table after sorting
+		eyes.checkWindow(" Table data after sorting ");
+
+		// End the test
+		eyes.closeAsync();
+	}
+
+	// Canvas Chart Test
+	@Test
+	public void test4_CanvasChartTest() {
+
+		// Start the test
+		eyes.open(driver, "Hackathon App", "Canvas Chart Test", new RectangleSize(1300, 580));
+
+		eyes.setForceFullPageScreenshot(true);
+
+		// Click on the Compare Expenses link
+		driver.findElement(By.id("showExpensesChart")).click();
+
+		// Take screenshot of data for the years 2017 and 2018
+		eyes.checkWindow("Data for the years 2017 and 2018");
+
+		// Click on the "Show data for next year" button
+		driver.findElement(By.id("addDataset")).click();
+
+		// Take screenshot of data for the years 2017, 2018 and 2019
+		eyes.checkWindow("Data for the years 2017, 2018 and 2019");
+
+		// End the test
+		eyes.closeAsync();
+	}
+
+	// Dynamic Content Test
+	@Test
+	public void test5_DynamicContentTest() {
+
+		// Set Layout Mode
+		eyes.setMatchLevel(MatchLevel.LAYOUT2);
+
+		// Start the test
+		eyes.open(driver, "Hackathon App", "Dynamic Content Test", new RectangleSize(1300, 580));
+
+		//driver.get("https://demo.applitools.com/hackathon.html?showAd=true");
+		driver.get(prop.getProperty(DYNAMIC_URL));
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		// Login to the application
+		validLogin();
+
+		// Take screenshot of both the gifs
+		eyes.checkWindow("For both flash sale gifs");
+
+		// End the test
+		eyes.closeAsync();
+	}
+
+	@DataProvider
+	public static Object[][] testData() {
+		return new Object[][] {
+				{ "", "", "Both Username and Password must be present",
+						"Login test - Both username and password fields empty" },
+				{ "someUserName", "", "Password must be present", "Login test - Password is empty" },
+				{ "", "somePassword", "Username must be present", "Login test - Username is empty" },
+				{ "someUserName", "somePassword", "Jack Gomez", "Login test - Success" } };
+
+	}
+
+	public void login(String username, String password, String testName) {
+		eyes.open(driver, "Hackathon App", testName, new RectangleSize(1300, 580));
+		driver.findElement(By.id("username")).clear();
+		driver.findElement(By.id("password")).clear();
+		driver.findElement(By.id("username")).sendKeys(username);
+		driver.findElement(By.id("password")).sendKeys(password);
+		driver.findElement(By.id("log-in")).click();
+		eyes.check(testName, Target.window().ignoreDisplacements());
+		eyes.closeAsync();
+
+	}
+
+	// Login with valid login credentials
+	public void validLogin() {
+
+		// Enter the username
+		driver.findElement(By.id("username")).sendKeys("someUserName");
+
+		// Enter only the password
+		driver.findElement(By.id("password")).sendKeys("somePassword");
+
+		// Click on the login button
+		driver.findElement(By.id("log-in")).click();
+	}
+
+	@AfterClass
+	public static void tearDown() {
+
+		// Close browser and eye
+		closeBrowserWindowAndEyes();
+
+		// Wait and collect all test results
+		allTestResults = runner.getAllTestResults();
+
+		// Print results
+		System.out.println(allTestResults);
+
+	}
+
+}
